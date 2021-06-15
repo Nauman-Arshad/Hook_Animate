1005158f6c862967476338d01e25fcbdbc9fc804
Nauman khan <60134017+Nauman-cloud@users.noreply.github.com>
2021-06-15 19:18:21 +0500

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+import Animation from "./animation";
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <Animation />
+    </div>
+  );
+}
